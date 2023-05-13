3f5ad41eaf5f7b1afedb3acb7926d550017586ec
Albert Shima <albertshima@gmail.com>
2023-05-13 23:28:44 +0200

    Adding more some features to teachAway.feature

--- src/test/java/com/teachAway/step_definitions/TeachAway_step_definitions.java
+++ src/test/java/com/teachAway/step_definitions/TeachAway_step_definitions.java
@@ -2,6 +2,7 @@ package com.teachAway.step_definitions;
 
 import com.teachAway.pages.JobsBoardPage;
 import com.teachAway.pages.TeachAwayPage;
+import com.teachAway.utilities.Driver;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
 
@@ -9,6 +10,12 @@ public class TeachAway_step_definitions {
     TeachAwayPage teachAwayPage = new TeachAwayPage();
     JobsBoardPage jobsBoardPage = new JobsBoardPage();
 
+
+    @Then("Verify that the title is {string}")
+    public void verifyThatTheTitleIs(String expectedTitle) {
+        String actualTitle = Driver.getDriver().getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
     @Then("you should see all menu´s of the main page")
     public void you_should_see_all_menu_s_of_the_main_page() {
         Assert.assertTrue(jobsBoardPage.jobs.isDisplayed());
@@ -17,7 +24,7 @@ public class TeachAway_step_definitions {
         Assert.assertTrue(teachAwayPage.destinations.isDisplayed());
         Assert.assertTrue(teachAwayPage.featuredPrograms.isDisplayed());
         Assert.assertTrue(teachAwayPage.jobOpenings.isDisplayed());
-        Assert.assertTrue(teachAwayPage.te.isDisplayed());
+
         Assert.assertTrue(teachAwayPage.tefl.isDisplayed());
 
         teachAwayPage.tefl.click();
@@ -34,4 +41,9 @@ public class TeachAway_step_definitions {
     }
 
 
+    @Then("Verify that the url is {string}")
+    public void verifyThatTheUrlIs(String expectedURL) {
+        String actualURL = Driver.getDriver().getCurrentUrl();
+        Assert.assertEquals(actualURL, expectedURL);
+    }
 }
--- src/test/resources/features/teachAway.feature
+++ src/test/resources/features/teachAway.feature
@@ -1,5 +1,16 @@
 Feature: Verifying all the menus from main page
 
+
+  Scenario: Verify the title
+    Given that you are on tech away site
+    Then Verify that the title is "Teach Abroad or Online | Your Dream Teaching Job Awaits"
+
+    Scenario: Verify the URL
+      Given that you are on tech away site
+      Then  Verify that the url is "https://www.teachaway.com/"
+
+
+
   Scenario: Verifying all the menu´s from main page
     Given that you are on tech away site
     Then you should see all menu´s of the main page
